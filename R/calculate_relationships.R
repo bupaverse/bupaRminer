a9beec77fd302a330c8bf70d78274016a4bba727
VANHOENSHOVEN Frank <frank.vanhoenshoven@uhasselt.be>
2023-03-17 14:03:09 +0100

    removed unnecessary files

--- R/calculate_relationships.R
+++ R/calculate_relationships.R
@@ -139,27 +139,3 @@ calculate_relationships <- function(ev_log,
   
   return(rel_df)
 }
-
-
-calculate_relationships_backup <- function(eventlog,
-                                    skip_self_loops = FALSE) {
-  rel_df <- calculate_relationship_scores(eventlog,
-                                          skip_self_loops)
-
-
-
-  # Set all rel values above threshold to 1
-  masked_df <- rel_df %>%
-    left_join(smart_thres_df) %>%
-    mutate(rounded_score = as.numeric(score >= rel_thres),
-           rel = factor(rel, levels = R_levels, ordered = TRUE)) %>%
-    select(-rel_thres)
-
-  # ## Filter out dominant relationship pased on R_levels
-  assigned_rel_df <- masked_df %>%
-    filter(rounded_score == 1) %>%
-    group_by(antecedent, consequent) %>%
-    arrange(rel) %>%
-    filter(row_number() == 1) %>%
-    ungroup()
-}
