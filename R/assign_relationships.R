4864beb38c88df84676ddaecbe38c7bff0f3314d
VANHOENSHOVEN Frank <frank.vanhoenshoven@uhasselt.be>
2023-03-22 01:43:01 +0100

    new rel selection

--- R/assign_relationships.R
+++ R/assign_relationships.R
@@ -11,7 +11,7 @@ assign_relationships <- function(relationships_df,
   } else{
     masked_df <- relationships_df %>%
       filter(score > 0) %>%
-      # group_by(rel) %>%
+      group_by(rel) %>%
       filter(score >= mean(score)) %>%
       mutate(score = round(score,1)) %>%
       group_by(antecedent, consequent) %>%
