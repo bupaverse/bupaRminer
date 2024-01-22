d366685f5622946420440fe6ce4b095925f4818d
VANHOENSHOVEN Frank <frank.vanhoenshoven@uhasselt.be>
2024-01-22 22:06:58 +0100

    changed loop replacement in log

--- R/solve_loop_blocks.R
+++ R/solve_loop_blocks.R
@@ -3,6 +3,9 @@ solve_loop_blocks <- function(loop_block_info_df, prep_log){
   
   relevant_snippets <- list()
   
+  loop_block_info_df <- loop_block_info_df %>%
+    clean_loop_blocks()
+  
   if(loop_block_info_df %>% nrow > 0){
     
     loop_blocks <- loop_block_info_df %>%
@@ -27,16 +30,34 @@ solve_loop_blocks <- function(loop_block_info_df, prep_log){
                is_end = ifelse(LC=="complete",is_end, FALSE)) %>%
         group_by(CID) %>%
         arrange(TS) %>%
-        mutate(nr_of_starts = cumsum(is_start)) %>%
-        mutate(starts_new_loop = (is_start & lag(is_end, default = TRUE))) %>%
-        group_by(CID, orig_name, LC) %>%
-        mutate(starts_per_act = cumsum(is_start)) %>%
-        group_by(CID,LC) %>%
-        mutate(start_counter = lag(cummax(starts_per_act), default=0)) %>%
-        mutate(starts_new_loop = (starts_new_loop | starts_per_act > start_counter)) %>%
-        group_by(CID) %>%
+        mutate(
+          status = ifelse(is_start, "searching_end",ifelse(is_end,"searching_start",NA))
+        ) %>%
+        fill(status, .direction = "down") %>%
+        fill(status, .direction = "up") %>%
+        mutate(next_status = lag(status)) %>%
+        mutate(ends_loop = (status != next_status & status == "searching_start" )) %>%
+        mutate(ends_loop = ifelse(is.na(ends_loop),FALSE, ends_loop)) %>%
+        mutate(starts_new_loop = lag(ends_loop, default = TRUE)) %>%
         mutate(loop_id = cumsum(starts_new_loop)) %>%
+        mutate(loop_id = loop_id) %>%
         ungroup()
+        
+        
+         
+      
+      ## mutate(nr_of_starts = cumsum(is_start)) %>%
+      ## mutate(nr_of_ends = cumsum(is_end))
+      ## loop_log <- loop_log %>%
+      ##  mutate(starts_new_loop = (is_start & lag(is_end, default = TRUE))) %>%
+      ##  group_by(CID, orig_name, LC) %>%
+      ##  mutate(starts_per_act = cumsum(is_start)) %>%
+      ##  group_by(CID,LC) %>%
+      ##  mutate(start_counter = lag(cummax(starts_per_act), default=0)) %>%
+      ##  mutate(starts_new_loop = (starts_new_loop | starts_per_act > start_counter)) %>%
+      ##  group_by(CID) %>%
+      ##  mutate(loop_id = cumsum(starts_new_loop)) %>%
+      ##  ungroup()
       
       
       first_loop_log <- loop_log %>%
@@ -123,4 +144,22 @@ solve_loop_blocks <- function(loop_block_info_df, prep_log){
   result$log <- prep_log
   
   return(result)
+}
+
+clean_loop_blocks <- function(loop_block_df){
+  no_end <- loop_block_df %>%
+    group_by(loop_block_id) %>%
+    summarize(nr_ends = sum(is_end)) %>%
+    filter(nr_ends == 0)
+  
+  no_start <- loop_block_df %>%
+    group_by(loop_block_id) %>%
+    summarize(nr_starts = sum(is_start)) %>%
+    filter(nr_starts == 0)
+  
+  new_loop_block_df <- loop_block_df %>%
+    mutate(is_start = ifelse(loop_block_id %in% no_end$loop_block_id & is_end == 0, 1, is_start))%>%
+    mutate(is_start = ifelse(loop_block_id %in% no_start$loop_block_id & is_start == 0, 1, is_end))
+  
+  return(new_loop_block_df)
 }
